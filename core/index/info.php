554c314d1e258fd8c261491595aff722491aec58
Mahammad <khalilovmaga05@gmail.com>
2023-04-01 18:17:54 +0200

    info part

--- core/index/info.php
+++ core/index/info.php
@@ -3,9 +3,23 @@
         <div class="row align-items-center">
             <div class="col-12 col-lg-6">
                 <div class="features-content">
-                    <h2>İnqilabi bir müəssisədə xəstələri müalicə etməyin yeni üsulu</h2>
-                    <p>İnqilabi müəssisə fərdi müalicə planlarına və bütöv müalicə təcrübələrinə üstünlük verən xəstələrə qulluq üçün unikal yanaşma təklif edir. Xəstələr ən müasir tibbi müalicələri, o cümlədən gen terapiyası və fərdiləşdirilmiş tibbin ən son üsullarını, həmçinin akupunktur və yoqa kimi bir sıra alternativ müalicələrə çıxışı gözləyə bilərlər. Müəssisənin yüksək təlim keçmiş tibb işçiləri hər bir xəstənin müalicə səyahəti boyu ən yüksək səviyyədə qayğı və dəstək almasını təmin etmək üçün yorulmadan çalışır.</p>
-                    <a href="#" class="btn medilife-btn mt-50">View the services <span>+</span></a>
+                    <h1>Sizdə süd vəzi xərçəngi ola bilər?</h1>
+                    <p>Döş xərçəngi həmişə erkən mərhələdə simptomlara səbəb olmaya bilər, buna görə də müntəzəm
+                        müayinələr vacibdir. Lakin xərçəng böyüdükcə ümumi simptomlar ola bilər:</p>
+                    <ul>
+                        <li>1. Döş və ya qoltuqaltı nahiyədə şiş və ya qalınlaşma</li>
+                        <li>2. Döşün ölçüsündə və ya formasında dəyişikliklər</li>
+                        <li>3. Döşdəki dərinin çökməsi və ya büzülməsi</li>
+                        <li>4. Döş başı axıntısı və ya içəriyə dönən məmə (ters çevrilmiş)</li>
+                        <li>5. Döş başı və ya döş dərisinin qızartı və ya qaşınması</li>
+                        <li>6. Döş və ya qoltuqaltı nahiyədə şişlik</li>
+                        <li>7. Döş və ya məmə nahiyəsində ağrı (baxmayaraq ki, bu daha az rast gəlinir)</li>
+                    </ul>
+                    <br>
+                    <p>Qeyd etmək vacibdir ki, bu simptomlar digər şərtlərdən də yarana bilər və
+                        bütün döş xərçəngi simptomlara səbəb olmur.
+                        Döşlərinizdə hər hansı dəyişiklik görsəniz, həkiminizlə danışmaq vacibdir.</p>
+                    <a href="#" class="btn medilife-btn mt-50"> Bütün xidmətlər <span>+</span></a>
                 </div>
             </div>
             <div class="col-12 col-lg-6">
