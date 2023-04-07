62a422ea569d55e04438b1499bb5f7fb9a8011e2
Mahammad <khalilovmaga05@gmail.com>
2023-04-07 10:45:19 +0200

    coming soon with preview

--- preview/core/index/gallery.php
+++ preview/core/index/gallery.php
@@ -2,31 +2,31 @@
 <div class="medilife-gallery-area owl-carousel">
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="preview/img/bg-img/d1.jpeg" alt="">
+        <img src="img/bg-img/d1.jpeg" alt="">
         <div class="view-more-btn">
-            <a href="preview/img/bg-img/d1.jpeg" class="btn gallery-img">See More +</a>
+            <a href="img/bg-img/d1.jpeg" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="preview/img/bg-img/d2.jpeg" alt="">
+        <img src="img/bg-img/d2.jpeg" alt="">
         <div class="view-more-btn">
-            <a href="preview/img/bg-img/d2.jpeg" class="btn gallery-img">See More +</a>
+            <a href="img/bg-img/d2.jpeg" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="preview/img/bg-img/d3.jpeg" alt="">
+        <img src="img/bg-img/d3.jpeg" alt="">
         <div class="view-more-btn">
-            <a href="preview/img/bg-img/d3.jpeg" class="btn gallery-img">See More +</a>
+            <a href="img/bg-img/d3.jpeg" class="btn gallery-img">See More +</a>
         </div>
     </div>
 
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="preview/img/bg-img/d4.jpeg" alt="">
+        <img src="img/bg-img/d4.jpeg" alt="">
         <div class="view-more-btn">
-            <a href="preview/img/bg-img/d4.jpeg" class="btn gallery-img">See More +</a>
+            <a href="img/bg-img/d4.jpeg" class="btn gallery-img">See More +</a>
         </div>
     </div>
 </div>
