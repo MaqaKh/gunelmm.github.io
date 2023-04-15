d9bb51db6b4e04b556eaaf8ceb6957db44d1a783
Mahammad <khalilovmaga05@gmail.com>
2023-04-15 22:40:49 +0200

    add gallry to about page

--- core/certificate-slider.php
+++ core/certificate-slider.php
@@ -1,203 +1,32 @@
-<section class="hero-area">
-    <div class="hero-slides owl-carousel">
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero1.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                           
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero2.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero3.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero4.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero5.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero6.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero7.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero8.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero9.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero10.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero11.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero12.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero13.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero14.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero15.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero16.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero17.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Single Hero Slide -->
-        <div class="single-hero-slide cert-img bg-overlay-white" style="background-image: url(/img/cert-img/hero18.jpg);">
-            <div class="container h-100">
-                <div class="row h-100 align-items-center">
-                    <div class="col-12">
-                        <div class="hero-slides-content">
-                        </div>
-                    </div>
-                </div>
-            </div>
+<!-- ***** Gallery Area Start ***** -->
+<div class="medilife-gallery-area owl-carousel">
+    <!-- Single Gallery Item -->
+    <div class="single-gallery-item">
+        <img src="/img/cert-img/hero1.jpg" alt="">
+        <div class="view-more-btn">
+            <a href="/img/cert-img/hero1.jpg" class="btn gallery-img">See More +</a>
         </div>
     </div>
-</section>
+    <!-- Single Gallery Item -->
+    <div class="single-gallery-item">
+        <img src="/img/cert-img/hero2.jpg" alt="">
+        <div class="view-more-btn">
+            <a href="/img/cert-img/hero2.jpg" class="btn gallery-img">See More +</a>
+        </div>
+    </div>
+    <!-- Single Gallery Item -->
+    <div class="single-gallery-item">
+        <img src="/img/cert-img/hero3.jpg" alt="">
+        <div class="view-more-btn">
+            <a href="/img/cert-img/hero3.jpg" class="btn gallery-img">See More +</a>
+        </div>
+    </div>
+
+    <!-- Single Gallery Item -->
+    <div class="single-gallery-item">
+        <img src="/img/cert-img/hero4.jpg" alt="">
+        <div class="view-more-btn">
+            <a href="/img/cert-img/hero4.jpg" class="btn gallery-img">See More +</a>
+        </div>
+    </div>
+</div>
