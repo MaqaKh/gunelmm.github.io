b4403184c38d537d803e36aececb8880785dd1ec
Mahammad <khalilovmaga05@gmail.com>
2023-04-28 07:37:04 +0200

    add new slider

--- core/index/slider.php
+++ core/index/slider.php
@@ -1,55 +1,71 @@
 <section class="hero-area">
     <div class="hero-slides owl-carousel">
         <!-- Single Hero Slide -->
-        <div class="single-hero-slide bg-img bg-overlay-white" style="background-image: url(/img/bg-img/uzman.png);">
+        <div class="single-hero-slide bg-img bg-overlay-white" style="background-color:#a0a29">
             <div class="container h-100">
                 <div class="row h-100 align-items-center">
                     <div class="col-12">
-                        <div class="hero-slides-content">
-                            <h2 data-animation="fadeInUp" data-delay="100ms">Uzman Doktor Günel Məmmədova</h2>
-                            <h6 data-animation="fadeInUp" data-delay="400ms"></h6>
-                            <div class="button-container">
-                                <button class="button"> Pasientlər üçün</button>
-                                <button class="button"> Həkimlər üçün</button>
+                        <div class="row hero-slides-content">
+                            <div class="col-md-6">
+                                <h2 data-animation="fadeInUp" data-delay="100ms">Uzman Doktor Günel Məmmədova</h2>
+                                <h6 data-animation="fadeInUp" data-delay="400ms"></h6>
+                            </div>
+                            <div class="col-md-6">
+                                <img class="slide-img" src="/img/bg-imgbg-img/uzman.png">
                             </div>
 
-                            <!--                            <a href="services.php" class="btn medilife-btn mt-50" data-animation="fadeInUp" data-delay="700ms"> Kəşf et <span>+</span></a>-->
+<!--                            <div class="button-container">-->
+<!--                                <button class="button"> Pasientlər üçün</button>-->
+<!--                                <button class="button"> Həkimlər üçün</button>-->
+<!--                            </div>-->
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         <!-- Single Hero Slide -->
-        <div class="single-hero-slide bg-img bg-overlay-white" style="background-image: url(/img/bg-img/hero_1_2.jpeg);">
+        <div class="single-hero-slide bg-img bg-overlay-white" style="background-color:#a0a29">
             <div class="container h-100">
                 <div class="row h-100 align-items-center">
                     <div class="col-12">
-                        <div class="hero-slides-content">
-                            <h2 data-animation="fadeInUp" data-delay="100ms">Keyfiyyətli tibbi xidmətdən  <br>rahatlıqla yararlanın</h2>
-                            <h6 data-animation="fadeInUp" data-delay="400ms"></h6>
-                            <div class="button-container">
-                                <button class="button"> Pasientlər üçün</button>
-                                <button class="button"> Həkimlər üçün</button>
+                        <div class="row hero-slides-content">
+                            <div class="col-md-6">
+                                <h2 data-animation="fadeInUp" data-delay="100ms">Keyfiyyətli tibbi xidmətdən  <br>rahatlıqla yararlanın</h2>
+                                <h6 data-animation="fadeInUp" data-delay="400ms"></h6>
                             </div>
-<!--                            <a href="services.php" class="btn medilife-btn mt-50" data-animation="fadeInUp" data-delay="700ms">Kəşf et <span>+</span></a>-->
+                            <div class="col-md-6">
+                                <img class="slide-img" src="/img/bg-imgbg-img/hero_1_2.jpeg">
+                            </div>
+
+<!--                            <div class="button-container">-->
+<!--                                <button class="button"> Pasientlər üçün</button>-->
+<!--                                <button class="button"> Həkimlər üçün</button>-->
+<!--                            </div>-->
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         <!-- Single Hero Slide -->
-        <div class="single-hero-slide bg-img bg-overlay-white" style="background-image: url(/img/bg-img/hero1_3.jpeg);">
+        <div class="single-hero-slide bg-img bg-overlay-white" style="background-color:#a0a29">
             <div class="container h-100">
                 <div class="row h-100 align-items-center">
                     <div class="col-12">
-                        <div class="hero-slides-content">
-                            <h2 data-animation="fadeInUp" data-delay="100ms">Etibar edə biləcəyiniz  <br>müstəsna tibbi xidmət</h2>
-                            <h6 data-animation="fadeInUp" data-delay="400ms"></h6>
-                            <div class="button-container">
-                                <button class="button"> Pasientlər üçün</button>
-                                <button class="button"> Həkimlər üçün</button>
+                        <div class="row hero-slides-content">
+                            <div class="col-md-6">
+                                <h2 data-animation="fadeInUp" data-delay="100ms">Etibar edə biləcəyiniz  <br>müstəsna tibbi xidmət</h2>
+                                <h6 data-animation="fadeInUp" data-delay="400ms"></h6>
                             </div>
-<!--                            <a href="services.php" class="btn medilife-btn mt-50" data-animation="fadeInUp" data-delay="700ms">Kəşf et<span>+</span></a>-->
+                            <div class="col-md-6">
+                                <img class="slide-img" src="/img/bg-imgbg-img/hero1_3.jpeg">
+
+                            </div>
+
+
+<!--                            <div class="button-container">-->
+<!--                                <button class="button"> Pasientlər üçün</button>-->
+<!--                                <button class="button"> Həkimlər üçün</button>-->
+<!--                            </div>-->
                         </div>
                     </div>
                 </div>
