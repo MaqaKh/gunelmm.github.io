a6344d3165878f6f73a63c3b77b3018b3cb44455
Mahammad <khalilovmaga05@gmail.com>
2023-04-30 08:15:26 +0200

    jpg to png

--- core/certificate-slider.php
+++ core/certificate-slider.php
@@ -2,136 +2,136 @@
 <div class="medilife-gallery-area owl-carousel">
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero1.jpg" alt="">
+        <img src="/img/cert-img/hero1.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero1.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero1.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero2.jpg" alt="">
+        <img src="/img/cert-img/hero2.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero2.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero2.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero3.jpg" alt="">
+        <img src="/img/cert-img/hero3.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero3.jpg" class="btn gallery-img">See More +</a>
-        </div>
-    </div>
-
-    <!-- Single Gallery Item -->
-    <div class="single-gallery-item">
-        <img src="/img/cert-img/hero4.jpg" alt="">
-        <div class="view-more-btn">
-            <a href="/img/cert-img/hero4.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero3.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
 
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero5.jpg" alt="">
+        <img src="/img/cert-img/hero4.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero5.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero4.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
 
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero6.jpg" alt="">
+        <img src="/img/cert-img/hero5.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero6.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero5.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
 
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero7.jpg" alt="">
+        <img src="/img/cert-img/hero6.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero7.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero6.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
 
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero8.jpg" alt="">
+        <img src="/img/cert-img/hero7.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero8.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero7.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
 
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero9.jpg" alt="">
+        <img src="/img/cert-img/hero8.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero9.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero8.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
 
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero10.jpg" alt="">
+        <img src="/img/cert-img/hero9.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero10.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero9.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
 
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero11.jpg" alt="">
+        <img src="/img/cert-img/hero10.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero11.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero10.png" class="btn gallery-img">See More +</a>
+        </div>
+    </div>
+
+    <!-- Single Gallery Item -->
+    <div class="single-gallery-item">
+        <img src="/img/cert-img/hero11.png" alt="">
+        <div class="view-more-btn">
+            <a href="/img/cert-img/hero11.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero12.jpg" alt="">
+        <img src="/img/cert-img/hero12.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero12.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero12.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero13.jpg" alt="">
+        <img src="/img/cert-img/hero13.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero13.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero13.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero14.jpg" alt="">
+        <img src="/img/cert-img/hero14.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero14.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero14.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero15.jpg" alt="">
+        <img src="/img/cert-img/hero15.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero15.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero15.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero16.jpg" alt="">
+        <img src="/img/cert-img/hero16.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero16.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero16.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero17.jpg" alt="">
+        <img src="/img/cert-img/hero17.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero17.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero17.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero18.jpg" alt="">
+        <img src="/img/cert-img/hero18.png" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero18.jpg" class="btn gallery-img">See More +</a>
+            <a href="/img/cert-img/hero18.png" class="btn gallery-img">See More +</a>
         </div>
     </div>
 </div>
