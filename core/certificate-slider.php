9e0b26bae4c81d88ffa01019ad961fb489f00708
Mahammad <khalilovmaga05@gmail.com>
2023-04-30 09:09:07 +0200

    remove imgs, add cdn

--- core/certificate-slider.php
+++ core/certificate-slider.php
@@ -2,23 +2,23 @@
 <div class="medilife-gallery-area owl-carousel">
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero1.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero9_10iq-1lJab.png?updatedAt=1682838216737" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero1.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero9_10iq-1lJab.png?updatedAt=1682838216737" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero2.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero10_lJI4kuK4i.png?updatedAt=1682838216601" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero2.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero10_lJI4kuK4i.png?updatedAt=1682838216601" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero3.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero8_HYBXb72I0.png?updatedAt=1682838216114" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero3.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero8_HYBXb72I0.png?updatedAt=1682838216114" class="btn gallery-img">See More +</a>
         </div>
     </div>
 
@@ -32,106 +32,106 @@
 
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero5.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero7_0q6oOWOVR.png?updatedAt=1682838215329" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero5.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero7_0q6oOWOVR.png?updatedAt=1682838215329" class="btn gallery-img">See More +</a>
         </div>
     </div>
 
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero6.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero17_3TVc_pbat.png?updatedAt=1682838198001" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero6.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero17_3TVc_pbat.png?updatedAt=1682838198001" class="btn gallery-img">See More +</a>
         </div>
     </div>
 
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero7.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero6_NKxJJjpC_.png?updatedAt=1682838212911" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero7.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero6_NKxJJjpC_.png?updatedAt=1682838212911" class="btn gallery-img">See More +</a>
         </div>
     </div>
 
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero8.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero5_8_PDgudR-.png?updatedAt=1682838212058" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero8.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero5_8_PDgudR-.png?updatedAt=1682838212058" class="btn gallery-img">See More +</a>
         </div>
     </div>
 
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero9.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero4_8dG3ZPZFx.png?updatedAt=1682838209942" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero9.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero4_8dG3ZPZFx.png?updatedAt=1682838209942" class="btn gallery-img">See More +</a>
         </div>
     </div>
 
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero10.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero3_H9va9MyQC.png?updatedAt=1682838204502" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero10.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero3_H9va9MyQC.png?updatedAt=1682838204502" class="btn gallery-img">See More +</a>
         </div>
     </div>
 
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero11.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero2_Bp0jl1kHT.png?updatedAt=1682838203353" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero11.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero2_Bp0jl1kHT.png?updatedAt=1682838203353" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero12.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero15_mLM4ChU6A.png?updatedAt=1682838203031" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero12.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero15_mLM4ChU6A.png?updatedAt=1682838203031" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero13.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero13_pNJNxRNrM.png?updatedAt=1682838202510" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero13.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero13_pNJNxRNrM.png?updatedAt=1682838202510" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero14.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero11_gkeafWSC6.png?updatedAt=1682838202294" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero14.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero11_gkeafWSC6.png?updatedAt=1682838202294" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero15.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero1_P1OFd-UGD.png?updatedAt=1682838202280" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero15.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero1_P1OFd-UGD.png?updatedAt=1682838202280" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero16.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero12_rlzQkyJmn.png?updatedAt=1682838202273" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero16.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero12_rlzQkyJmn.png?updatedAt=1682838202273" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero17.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero16_E8KcH9KBY.png?updatedAt=1682838200366" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero17.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero16_E8KcH9KBY.png?updatedAt=1682838200366" class="btn gallery-img">See More +</a>
         </div>
     </div>
     <!-- Single Gallery Item -->
     <div class="single-gallery-item">
-        <img src="/img/cert-img/hero18.png" alt="">
+        <img src="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero14__A1RN2yy-.png?updatedAt=1682838198005" alt="">
         <div class="view-more-btn">
-            <a href="/img/cert-img/hero18.png" class="btn gallery-img">See More +</a>
+            <a href="https://ik.imagekit.io/rwrqa8er9rmp/sudvezi/hero14__A1RN2yy-.png?updatedAt=1682838198005" class="btn gallery-img">See More +</a>
         </div>
     </div>
 </div>
